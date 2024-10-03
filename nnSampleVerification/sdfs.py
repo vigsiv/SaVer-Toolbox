b954f363147ddcb308686e547508ceb8a0c176ba
vigsiv <vigsiv@unm.edu>
2024-10-02 18:50:09 -0700

    file restructuring and adding taxinet

--- nnSampleVerification/sdfs.py
+++ nnSampleVerification/sdfs.py
@@ -2,19 +2,20 @@ import numpy as np
 
 
 class norm:
-    def __init__(self,center):
+    def __init__(self,center,norm=2):
         
         self.center = center
+        self.norm = norm
 
     def reviseCenter(self,center):
         self.center = center
 
-    def eval(self, point, zero_radius, norm=2):
+    def eval(self, point, zero_radius):
         # Calculate the signed distance of a point from the set
         # Replace this with your actual signed distance function implementation
 
         eval = np.zeros(point.shape[0])
 
-        eval = np.linalg.vector_norm(point-self.center,axis=1,ord=2) - zero_radius
+        eval = np.linalg.vector_norm(point-self.center,axis=1,ord=self.norm) - zero_radius
 
         return eval
