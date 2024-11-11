aeb3e402d209a71f7c7e6f1d0b4aa4858156a22c
Krishna Chaitanya  Kalagarla <krishnachaitanyakalagarla@Krishnas-MacBook-Pro.local>
2024-11-11 09:44:00 -0700

    Changes to CNN examples. Changed findZeroOne method of library.

--- nnSampleVerification/verifMethods.py
+++ nnSampleVerification/verifMethods.py
@@ -17,19 +17,13 @@ class dkw:
     def addSamples(self,samples):
         self.samples = samples
 
-    def findZeroOne(self,initPoint):
-
-        evalCheck = self.empiricalCDF(initPoint)
-
-        if evalCheck == 1.0 or evalCheck == 0.0:
-            raise ValueError("Choose a different initial point")
-
+    def findZeroOne(self):
         self.zeroOne = np.zeros(2)
         self.tol = 1e-12
         # Find the zero crossing
-        self.zeroOne[0] = optimize.root_scalar(self.empiricalCDF, x0=initPoint, x1=initPoint+0.1, xtol=self.tol).root
+        self.zeroOne[0] = optimize.root_scalar(self.empiricalCDF, method = 'brentq', bracket = [-100,100], xtol=self.tol).root
         complementZero = lambda x: 1-self.empiricalCDF(x)
-        self.zeroOne[1] = optimize.root_scalar(complementZero, x0=initPoint, x1=initPoint-0.1, xtol=self.tol).root
+        self.zeroOne[1] = optimize.root_scalar(complementZero,method = 'brentq', bracket = [-100,100], xtol=self.tol).root
 
     def empiricalCDF(self,eval):
         # Calculate the empirical CDF of the signed distance function
