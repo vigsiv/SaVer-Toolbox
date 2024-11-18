73f91e9cf4699cd125c297594e845abd7baaf60b
vigsiv <vigsiv@unm.edu>
2024-11-17 16:06:51 -0800

    specified pytorch version

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nnSampleVerification',
+    name='SaVer Toolbox',
     version='0.1',
     packages=find_packages(),
     install_requires=[
         'matplotlib',   # For plotting
         'numpy',        # For numerical calculations
-        'torch',        # PyTorch
+        'torch>=1.7.0', # PyTorch
         'cvxpy',        # For optimization
         'torchvision',  # For image processing
         'h5py',         # For reading and writing HDF5 files
